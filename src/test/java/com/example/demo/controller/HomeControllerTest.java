675418c140920c35ff9b01b16dac9bf830016609
Ashok Kumar Reddy <mail.ashokreddy@gmail.com>
2018-04-08 17:01:03 +0530

    Sonar integration

--- src/test/java/com/example/demo/controller/HomeControllerTest.java
+++ src/test/java/com/example/demo/controller/HomeControllerTest.java
@@ -4,19 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { HomeController.class })
 public class HomeControllerTest {
-	
+
 	@Mock
 	HomeController homeController;
-	
+
 	@Test
 	public void testHomeController() {
 		homeController.homeController();
-		Assert.assertEquals(true, true); 
+		Assert.assertEquals(true, true);
 	}
 
 }
